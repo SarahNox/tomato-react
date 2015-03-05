261140cf6e420bf2c466c720a3f6384a10aa38d2
SarahNox <apnoeist@googlemail.com>
2015-03-05 18:54:18 +0100

    clean up

--- app/controllers/tomato_controller.rb
+++ app/controllers/tomato_controller.rb
@@ -4,11 +4,5 @@ class TomatoController < ApplicationController
   end
 
   def new
-  	minutes = 25
-  	seconds = minutes * 60
-  	start_time = Time.now
-  	end_time = start_time + seconds
-  	puts start_time
-  	puts end_time
   end
 end
